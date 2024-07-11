17e5dde7ed08c57327354152e8c8d169a02176b6
AndrDragomir <andrei.dragomir@analog.com>
2024-07-11 17:36:46 +0300

    projects/ad5592r_pmdz: Add template
    Signed-off-by: AndrDragomir <andrei.dragomir@analog.com>

---
+++ projects/ad5592r_pmdz/common/ad5592r_pmdz_bd.tcl
@@ -0,0 +1,28 @@
+# 1. create ports: pwm_led_0_r, pwm_led_0_g, pwm_led_0_b, pwm_led_1_r, pwm_led_1_g, pwm_led_1_b ports
+    # command example: create_bd_port -dir O example led
+
+  #here
+  #here
+  #here
+
+  #here
+  #here
+  #here
+
+# 2. add axi_pwm_custom IP using ad_ip_instance command
+
+  #here
+
+# 3. connect the axi_pwm_custom IP to the block design ports using ad_connect command
+
+  #here
+  #here
+  #here
+
+  #here
+  #here
+  #here
+
+# 4. connect the axi_pwm_custom IP to the CPU using ad_cpu_interconnect at 0x44a00000
+
+  #here
