d5f3dac3be195417f0af792d29d5d663c7a4170c
hadarauelena24 <analog@DESKTOP-SCUQ0E3.>
2024-07-29 16:55:59 +0300

    Add axi_pwm_custom
    Signed-off-by: hadarauelena24 <analog@DESKTOP-SCUQ0E3.>

--- library/axi_pwm_custom/axi_pwm_custom_if.v
+++ library/axi_pwm_custom/axi_pwm_custom_if.v
@@ -58,29 +58,113 @@ module axi_pwm_custom_if (
 
   // internal registers
 
-  /*here*/
+  reg [11:0]cnt=12'hFFF;
+  wire END_OF_PERIOD;
+  reg l0=1'b0;
+  reg l1=1'b0;
+  reg l2=1'b0;
+  reg l3=1'b0;
+  reg l4=1'b0;
+  reg l5=1'b0;
+  reg [11:0]d0=12'h000;
+  reg [11:0]d1=12'h000;
+  reg [11:0]d2=12'h000;
+  reg [11:0]d3=12'h000;
+  reg [11:0]d4=12'h000;
+  reg [11:0]d5=12'h000;
+
 
   // internal wires
 
   /*here*/
 
+  // Create a counter from 0 to PULSE_PERIOD
+
+  always @(posedge pwm_clk)
+    begin
+      if(rstn==0)
+        cnt<=0;
+      else
+        if(END_OF_PERIOD == 1'b1)
+          cnt<=0;
+        else
+          cnt<=cnt+1;
+    end
+
 // generate a signal named end_of_period which has '1' logic value at the end of the signal period
 
-  /*here*/
+  assign END_OF_PERIOD=(cnt==PULSE_PERIOD)?1'b1:1'b0;
 
+// make sure that the new data is processed only after the END_OF_PERIOD
+always @(posedge pwm_clk)
+    begin
+       if(END_OF_PERIOD == 1'b1) begin 
+        d0 <= data_channel_0;
+        d1 <= data_channel_1;
+        d2 <= data_channel_2;
+        d3 <= data_channel_3;
+        d4 <= data_channel_4;
+        d5 <= data_channel_5;
+        end
 
-// Create a counter from 0 to PULSE_PERIOD
-
-  /*here*/
+    end
 
 
 // control the pwm signal value based on the input signal and counter value
 
-  /*here*/
+  always @(posedge pwm_clk)
+    begin
+      if(cnt<d0)
+        l0<=1'b1;
+      else
+        l0<=1'b0;
+    end
+  assign pwm_led_0=l0;
 
-// make sure that the new data is processed only after the END_OF_PERIOD
+  always @(posedge pwm_clk)
+    begin
+      if(cnt<d1)
+        l1<=1'b1;
+      else
+        l1<=1'b0;
+    end
+  assign pwm_led_1=l1;
+  
+  always @(posedge pwm_clk)
+    begin
+      if(cnt<d2)
+        l2<=1'b1;
+      else
+        l2<=1'b0;
+    end
+  assign pwm_led_2=l2;
 
-  /*here*/
+  always @(posedge pwm_clk)
+    begin
+      if(cnt<d3)
+        l3<=1'b1;
+      else
+        l3<=1'b0;
+    end
+  assign pwm_led_3=l3;
+
+  always @(posedge pwm_clk)
+    begin
+      if(cnt<d4)
+        l4<=1'b1;
+      else
+        l4<=1'b0;
+    end
+  assign pwm_led_4=l4;
+
+always @(posedge pwm_clk)
+    begin
+      if(cnt<d5)
+        l5<=1'b1;
+      else
+        l5<=1'b0;
+    end
+  assign pwm_led_5=l5;
 
 
 endmodule
