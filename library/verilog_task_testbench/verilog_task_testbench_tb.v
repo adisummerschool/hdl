6b2350d4427c0f2184e5a64706522441a62996a8
Cristian Mihai Popa <cristianmihai.popa@analog.com>
2025-07-28 16:37:11 +0300

    library/verilog_task_testbench: Add triangular waveform generation
    - Add a testbench for triangular waveform generation, found in verilog_task_testbench_tb.v, which modulates a triangle wave
    - Add a 5ns clock refresh rate
    - Add an active low synchronous reset that toggles after 100ns
    - Add a 12 bit triangular waveform generator in verilog_task_testbench.v. A counter and a register that toggles between 0 and 1 was used to figure out the direction of the waveform (rising or falling edge) that counts up to 2^12 from 0 and vice versa.
    - Add an image that showcases the result of the code as run in Vivado, waveform.png
    
    Signed-off-by: Evelyn Plesca <evelyn-iulia.plesca@analog.com>

---
+++ library/verilog_task_testbench/verilog_task_testbench_tb.v
@@ -0,0 +1,60 @@
+// ***************************************************************************
+// ***************************************************************************
+// Copyright 2022 - 2023(c) Analog Devices, Inc. All rights reserved.
+//
+// In this HDL repository, there are many different and unique modules, consisting
+// of various HDL(Verilog or VHDL) components. The individual modules are
+// developed independently, and may be accompanied by separate and unique license
+// terms.
+//
+// The user should read each of these license terms, and understand the
+// freedoms and responsibilities that he or she has by using this source/core.
+//
+// This core is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+// A PARTICULAR PURPOSE.
+//
+// Redistribution and use of source or resulting binaries, with or without modification
+// of this file, are permitted under one of the following two license terms:
+//
+//   1. The GNU General Public License version 2 as published by the
+//      Free Software Foundation, which can be found in the top level directory
+//      of this repository(LICENSE_GPL2), and also online at:
+//      <https://www.gnu.org/licenses/old-licenses/gpl-2.0.html>
+//
+// OR
+//
+//   2. An ADI specific BSD license, which can be found in the top level directory
+//      of this repository(LICENSE_ADIBSD), and also on-line at:
+//      https://github.com/analogdevicesinc/hdl/blob/master/LICENSE_ADIBSD
+//      This will allow to generate bit files and not release the source code,
+//      as long as it attaches to an ADI device.
+//
+// ***************************************************************************
+// ***************************************************************************
+
+`timescale 1ns/100ps
+
+module verilog_task_testbench_tb;
+  parameter VCD_FILE = "verilog_task_testbench_tb.vcd";
+
+  `define TIMEOUT 900
+
+  reg ref_clk;
+  reg rstn = 0;
+  wire[11:0] triangle_wave;
+
+  initial begin
+    ref_clk = 0;
+    rstn = 0;
+    
+    #100 rstn = 1;  // deassert reset after 100us
+
+    #100000 $finish;
+  end
+
+  always #5 ref_clk = ~ref_clk;  // toggle every 5ms
+
+  verilog_task_testbench dut(ref_clk, rstn, triangle_wave);
+
+endmodule
