b59fc2d9f1f927747f13632f6b836b2e67404c5c
Alexandru Cimpean <cimpeanalexandru11@gmail.com>
2025-07-31 13:59:53 +0300

    library/axi_pwm_custom: PWM
    - Routed all the missing signals
    - Implemented a counter and compared its outputs in order to generate a PWM signal
    - Used a signal 'end_of_period' to enable data channels
    - Attached a photo that present PWM waveform and other useful signals
    
    Signed-off-by: Alexandru Cimpean <cimpeanalexandru11@gmail.com>

--- library/axi_pwm_custom/axi_pwm_custom_if.v
+++ library/axi_pwm_custom/axi_pwm_custom_if.v
@@ -58,30 +58,82 @@ module axi_pwm_custom_if (
 
 // internal registers
 
-  /*here*/
+  reg [11:0] counter = 12'h000;
+  reg [5:0] pwms = 12'b00000;
+  reg [11:0] inputs [0:5];
 
 // internal wires
 
-  /*here*/
+  wire end_of_period;
 
 // generate a signal named end_of_period which has '1' logic value at the end of the signal period
 
-  /*here*/
+  assign end_of_period = (counter == 12'd4095) ? 1'b1 : 1'b0;
 
 // Create a counter from 0 to PULSE_PERIOD
 
-  /*here*/
+  always @(posedge pwm_clk) begin
+    if(!rstn) begin
+      counter <= 12'h000;
+    end else begin
+      counter <= counter + 1'b1;
+    end
+  end
 
 // control the pwm signal value based on the input signal and counter value
 
-  /*here*/
+  always @(posedge pwm_clk) begin
+    if(!rstn) begin
+      pwms[0] <= 1'b0;
+      pwms[1] <= 1'b0;
+      pwms[2] <= 1'b0;
+      pwms[3] <= 1'b0;
+      pwms[4] <= 1'b0;
+      pwms[5] <= 1'b0;
+    end else begin
+      pwms[0] <= (counter < inputs[0]) ? 1'b1 : 1'b0;
+      pwms[1] <= (counter < inputs[1]) ? 1'b1 : 1'b0;
+      pwms[2] <= (counter < inputs[2]) ? 1'b1 : 1'b0;
+      pwms[3] <= (counter < inputs[3]) ? 1'b1 : 1'b0;
+      pwms[4] <= (counter < inputs[4]) ? 1'b1 : 1'b0;
+      pwms[5] <= (counter < inputs[5]) ? 1'b1 : 1'b0;
+    end
+  end
 
 // make sure that the new data is processed only after the END_OF_PERIOD
 
-  /*here*/
+  always @(posedge pwm_clk) begin
+    if(!rstn) begin
+      inputs[0] <= 12'd0;
+      inputs[1] <= 12'd0;
+      inputs[2] <= 12'd0;
+      inputs[3] <= 12'd0;
+      inputs[4] <= 12'd0;
+      inputs[5] <= 12'd0;
+    end else if (end_of_period) begin
+      inputs[0] <= data_channel_0;
+      inputs[1] <= data_channel_1;
+      inputs[2] <= data_channel_2;
+      inputs[3] <= data_channel_3;
+      inputs[4] <= data_channel_4;
+      inputs[5] <= data_channel_5;
+    end else begin
+        inputs[0] <= inputs[0];  
+        inputs[1] <= inputs[1];
+        inputs[2] <= inputs[2];
+        inputs[3] <= inputs[3];
+        inputs[4] <= inputs[4];
+        inputs[5] <= inputs[5];
+      end
+    end
 
 // continous assigment of the correct PWM value for the LEDs
 
- /*here*/
+  assign pwm_led_0 = pwms[0];
+  assign pwm_led_1 = pwms[1];
+  assign pwm_led_2 = pwms[2];
+  assign pwm_led_3 = pwms[3];
+  assign pwm_led_4 = pwms[4];
+  assign pwm_led_5 = pwms[5];
 
 endmodule
