441fc7cf409fa79b2c151e6acfbc4c37f1864fc0
Cosmin Popa <popa.gi.cosmin@student.utcluj.ro>
2025-07-31 14:11:13 +0300

    library/axi_pwm_custom: Completed and simulated the pwm module
    Completed the PWM module by editing this 2 files:
    - axi_pwm_custom.v: added the connection samples for every data_channel, in order to make sure of the connection between the larger module of the PWM and the input
    from the ADC
    - axi_pwm_custom_if: added the whole logic of the pwm signal. The signal is generated using a counter with an end_of period signal, in order to control the fact that
    the data from the adc should be processed only after the end of the pwm period. The pwm will be visible on the leds of the fpga, which are connected at the end of
    this file. The turning on and off logic can be seen at the always loop, where all 6 pwm signals are controlled using the values of the count.
    
    The simulation of the PWM can be seen in the photo attached, PWM_Cosmin. The simulation was made using Vivado and the signals can be seen at the end, 6 signals which
    are high for shorter and increasingly higher, and after saturation, it restarts as zero and re-enters the loop
    
    Signed-off-by: Cosmin Popa <popa.gi.cosmin@student.utcluj.ro>

--- library/axi_pwm_custom/axi_pwm_custom_if.v
+++ library/axi_pwm_custom/axi_pwm_custom_if.v
@@ -46,41 +46,178 @@ module axi_pwm_custom_if (
   input    [11:0]  data_channel_3,
   input    [11:0]  data_channel_4,
   input    [11:0]  data_channel_5,
-  output           pwm_led_0,
-  output           pwm_led_1,
-  output           pwm_led_2,
-  output           pwm_led_3,
-  output           pwm_led_4,
-  output           pwm_led_5
+  output reg         pwm_led_0,
+  output reg          pwm_led_1,
+  output reg          pwm_led_2,
+  output reg          pwm_led_3,
+  output reg          pwm_led_4,
+  output reg          pwm_led_5
 );
 
   localparam PULSE_PERIOD = 4095;
 
 // internal registers
 
-  /*here*/
+  reg [11:0]count;
+  reg end_of_period;
+  reg [11:0]pwm_signal;
+  reg in_signal;
 
 // internal wires
 
-  /*here*/
+  reg [11:0]internal0;
+  reg [11:0]internal1;
+  reg [11:0]internal2;
+  reg [11:0]internal3;
+  reg [11:0]internal4;
+  reg [11:0]internal5;
 
+  reg  aux_pwm_led_0;
+  reg  aux_pwm_led_1;
+  reg  aux_pwm_led_2;
+  reg  aux_pwm_led_3;
+  reg  aux_pwm_led_4;
+  reg  aux_pwm_led_5;
+
+
+  
 // generate a signal named end_of_period which has '1' logic value at the end of the signal period
+//always@(posedge clk)
+//if(count == 3'h111) begin
+//end_of_period <= 1'b1;  
+//end
 
-  /*here*/
+  
 
 // Create a counter from 0 to PULSE_PERIOD
 
   /*here*/
+  always@(posedge pwm_clk) begin
+
+  if(!rstn) begin
+
+    count <= 12'b000000000000;
+    end_of_period <= 0;
+  
+  end  else begin
+  
+    if (count < 12'b111111111111) begin
+
+    count <= count + 1;  
+    end_of_period <= 1;
+  
+  end else begin
+  
+    count <= 12'b000000000000;
+    end_of_period <= 0;
+  
+  end
+  end
+end
 
 // control the pwm signal value based on the input signal and counter value
 
-  /*here*/
+  always@(posedge pwm_clk)begin
+
+  if(internal0 > count) begin
+
+    aux_pwm_led_0 <= 1'b1;
+  
+  end else begin
+  
+    aux_pwm_led_0 <= 1'b0;
+  
+  end
+  
+  if(internal1 > count) begin
+  
+    aux_pwm_led_1 <= 1'b1;
+  
+  end else begin
+  
+    aux_pwm_led_1 <= 1'b0;
+  
+  end
+  
+  if(internal2 > count) begin
+  
+    aux_pwm_led_2 <= 1'b1;
+  
+  end else begin
+  
+    aux_pwm_led_2 <= 1'b0;
+  
+  end
+  
+  if(internal3 > count) begin
+  
+    aux_pwm_led_3 <= 1'b1;
+  
+  end else begin
+  
+    aux_pwm_led_3 <= 1'b0;
+  
+  end
+  
+  if(internal4 > count) begin
+  
+    aux_pwm_led_4 <= 1'b1;
+  
+  end else begin
+  
+    aux_pwm_led_4 <= 1'b0;
+  
+  end
+  
+  if(internal5 > count) begin
+  
+    aux_pwm_led_5 <= 1'b1;
+  
+  end else begin
+  
+    aux_pwm_led_5 <= 1'b0;
+  
+  end
+  end
+
 
 // make sure that the new data is processed only after the END_OF_PERIOD
 
   /*here*/
+  always@(posedge pwm_clk) begin
+  if(end_of_period == 1'b1) begin
+
+      internal0 <= data_channel_0;
+      internal1 <= data_channel_1;
+      internal2 <= data_channel_2;
+      internal3 <= data_channel_3;
+      internal4 <= data_channel_4;  
+      internal5 <= data_channel_5;
+  
+    end else begin
+  
+      internal0 <= internal0;
+      internal1 <= internal1;
+      internal2 <= internal2;
+      internal3 <= internal3;
+      internal4 <= internal4;  
+      internal5 <= internal5;
+  
+    end
+  
+  end
 
 // continous assigment of the correct PWM value for the LEDs
+  always@(posedge pwm_clk) begin
+
+    pwm_led_0 <= aux_pwm_led_0;
+    pwm_led_1 <= aux_pwm_led_1;
+    pwm_led_2 <= aux_pwm_led_2;
+    pwm_led_3 <= aux_pwm_led_3;
+    pwm_led_4 <= aux_pwm_led_4;
+    pwm_led_5 <= aux_pwm_led_5;
+
+  end
 
  /*here*/
 
