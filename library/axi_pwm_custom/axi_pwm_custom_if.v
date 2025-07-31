e83c95a4f18181852da83360af811067a8241f0e
Andreea Anastasiu <aanastasiu52@gmail.com>
2025-07-31 12:58:58 +0300

    library/axi_pwm_custom: Axi for pwm
    In axi_pwm_custom I genereted signals to control 6 leds. I implement 12 bits counter that cycle
    from 0 to 4095, generate pwm and update the values. Counter increments on each clk cycle
    Each Led output is 1 when counter< inp value
    Input data is buffered and updated only at period boundaries
    In axi_pwm_custom: completed with output signals (pwm_led_1) and mapping the signals axi_pwm_custom_if
    to axi_pwm_custom.
    In photo see the signals and waveforme.
    
    Signed-off-by: Andreea Anastasiu <aanastasiu52@gmail.com>

--- library/axi_pwm_custom/axi_pwm_custom_if.v
+++ library/axi_pwm_custom/axi_pwm_custom_if.v
@@ -47,41 +47,138 @@ module axi_pwm_custom_if (
   input    [11:0]  data_channel_4,
   input    [11:0]  data_channel_5,
   output           pwm_led_0,
-  output           pwm_led_1,
-  output           pwm_led_2,
+  output          pwm_led_1,
+  output          pwm_led_2,
   output           pwm_led_3,
-  output           pwm_led_4,
-  output           pwm_led_5
+  output          pwm_led_4,
+  output          pwm_led_5
 );
 
   localparam PULSE_PERIOD = 4095;
 
 // internal registers
+  reg [11:0] cnt=12'h000;
+  reg end_per;
+  reg [11:0]aux_data_0;
+  reg [11:0]aux_data_1;
+  reg [11:0]aux_data_2;
+  reg [11:0]aux_data_3;
+  reg [11:0]aux_data_4;
+  reg [11:0]aux_data_5;
 
   /*here*/
 
 // internal wires
+  reg l0,l1, l2, l3, l4, l5;
 
   /*here*/
 
 // generate a signal named end_of_period which has '1' logic value at the end of the signal period
 
   /*here*/
+  always @(posedge pwm_clk) begin
+   end_per=(cnt==PULSE_PERIOD)? 1:0;
+  end
 
 // Create a counter from 0 to PULSE_PERIOD
+  always @(posedge pwm_clk) begin
+    if(rstn==1'b0)begin
+      l0<=1'b0;
+      l1<=1'b0;
+      l2<=1'b0;
+      l3<=1'b0;
+      l4<=1'b0;
+      l5<=1'b0;
+      cnt<=12'd0;
+
+    end
+    else begin
+      if(cnt==PULSE_PERIOD) begin
+        cnt<=12'd0;
+        
+      end
+      else
+          cnt<=cnt+12'd1;
+
+    end
+
+  end
 
   /*here*/
 
 // control the pwm signal value based on the input signal and counter value
+  always @(posedge pwm_clk)begin
+    if(cnt < aux_data_0) begin
+      l0<=1'b1;
+    end
+    else
+       l0<=1'b0;
+    if(cnt < aux_data_1) begin
+      l1<=1'b1;
+    end
+    else
+       l1<=1'b0;
+    if(cnt < aux_data_2) begin
+      l2<=1'b1;
+    end
+    else
+       l2<=1'b0;
+     if(cnt < aux_data_3) begin
+      l3<=1'b1;
+    end
+    else
+      l3<=1'b0;
+    if(cnt < aux_data_4) begin
+      l4<=1'b1;
+    end
+    else
+       l4<=1'b0;
+    if(cnt < aux_data_5) begin
+      l5<=1'b1;
+    end
+    else
+       l5<=1'b0;
+  end
 
   /*here*/
 
 // make sure that the new data is processed only after the END_OF_PERIOD
+  always @(posedge pwm_clk) begin
+    if(end_per) begin
+      aux_data_0<=data_channel_0;
+      aux_data_1<=data_channel_0;
+      aux_data_2<=data_channel_0;
+      aux_data_3<=data_channel_0;
+      aux_data_4<=data_channel_0;
+      aux_data_5<=data_channel_0;
+    
+
+    end
+    else begin
+      aux_data_0<=aux_data_0;
+      aux_data_1<=aux_data_1;
+      aux_data_2<=aux_data_2;
+      aux_data_3<=aux_data_3;
+      aux_data_4<=aux_data_4;
+      aux_data_5<=aux_data_5;
+    end
+  end
+
 
   /*here*/
 
 // continous assigment of the correct PWM value for the LEDs
 
  /*here*/
+  assign pwm_led_0=l0;
+  assign pwm_led_1=l1;
+  assign pwm_led_2=l2;
+  assign pwm_led_3=l3;
+  assign pwm_led_4=l4;
+  assign pwm_led_5=l5;
+
+
+  
+
 
 endmodule
