884b104bccd15a10aa9740ccfdef909a90b3609b
Eric Serban <e.serban2003@gmail.com>
2025-07-31 15:50:06 +0300

    peojects/ad5592r_pmdz: AXI CPU Interconnect
    - Linked axi_pwm_custom IP to the block design ports and the PS microprocessor
    in the .tcl file
    - Declared the block design ports and connected them to the PWM wires in the
    system_top.v file
    - Connected the PWM wires to the imput port of the ad_iobuf buffer in the system_top.v file
    - Implemented the SPI protocol and the sniffing signals in the system_top.v file
    - Connected the ports from the project with the physical pins on the board in
    the .xdc constraints file
    
    Signed-off-by: Eric Serban <e.serban2003@gmail.com>

--- projects/ad5592r_pmdz/coraz7s/system_top.v
+++ projects/ad5592r_pmdz/coraz7s/system_top.v
@@ -65,14 +65,17 @@ module system_top (
 
 // 4. Add SPI ports for both ADC PMOD connector and sniffing PMOD connector 
   /*here*/
-  /*here*/
-  /*here*/
-  /*here*/
+  output cs,
+  output sclk,
+  output mosi,
+  input miso,
 
   /*here*/
-  /*here*/
-  /*here*/
-  /*here*/
+  output sniffing_cs,
+  output sniffing_sclk,
+  output sniffing_mosi,
+  output sniffing_miso
+
 );
 
   // internal signals
@@ -83,13 +86,7 @@ module system_top (
  
 // 3. Declare the PWM wires that controls the LED's
 
-  /*here*/
-  /*here*/
-  /*here*/
-  
-  /*here*/
-  /*here*/
-  /*here*/
+  wire pwm_led_0_r, pwm_led_0_g, pwm_led_0_b, pwm_led_1_r, pwm_led_1_g, pwm_led_1_b;
 
   // instantiations
   ad_iobuf #(
@@ -110,7 +107,13 @@ module system_top (
              /*here*/           //led[3]
              /*here*/           //led[2]
              /*here*/           //led[1]
-             /*here*/}),        //led[0]
+             /*here*/           //led[0]
+              pwm_led_1_g,
+              pwm_led_1_r,
+              pwm_led_1_b,
+              pwm_led_0_g,        
+              pwm_led_0_r,
+              pwm_led_0_b }),
     .dio_o (gpio_i[7:2]),
     .dio_p (led));
 
@@ -120,9 +123,10 @@ module system_top (
 // 6. Clone the ADC SPI port to the sniffing ports 
 
   /*here*/
-  /*here*/
-  /*here*/
-  /*here*/
+  assign sniffing_sclk = sclk;
+  assign sniffing_cs = cs;
+  assign sniffing_mosi = mosi;
+  assign sniffing_miso = miso;
 
   system_wrapper i_system_wrapper (
     .ddr_addr (ddr_addr),
@@ -153,14 +157,14 @@ module system_top (
     .gpio_t (gpio_t),
 
     .spi0_clk_i (),
-    .spi0_clk_o (/*here*/),   // 5. Connect here the SPI CLK
-    .spi0_csn_0_o (/*here*/),   // 5. Connect here the SPI CS
+    .spi0_clk_o (sclk),   // 5. Connect here the SPI CLK
+    .spi0_csn_0_o (cs),   // 5. Connect here the SPI CS
     .spi0_csn_1_o (),
     .spi0_csn_2_o (),
     .spi0_csn_i (1'b1),
-    .spi0_sdi_i (/*here*/),   // 5. Connect here the SPI MISO
+    .spi0_sdi_i (miso),   // 5. Connect here the SPI MISO
     .spi0_sdo_i (),
-    .spi0_sdo_o (/*here*/),   // 5. Connect here the SPI MOSI
+    .spi0_sdo_o (mosi),   // 5. Connect here the SPI MOSI
     .spi1_clk_i (1'b0),
     .spi1_clk_o (),
     .spi1_csn_0_o (),
@@ -171,11 +175,12 @@ module system_top (
     .spi1_sdo_i (1'b0),
     .spi1_sdo_o (),
 // 1. Declare the block design ports and connect them to the PWM wires 
-    /*here*/
-    /*here*/
-    /*here*/
-    /*here*/
-    /*here*/
-    /*here*/);
+    .pwm_led_0_r (pwm_led_0_r),
+    .pwm_led_0_g (pwm_led_0_g),
+    .pwm_led_0_b (pwm_led_0_b),
+    .pwm_led_1_r (pwm_led_1_r),
+    .pwm_led_1_g (pwm_led_1_g),
+    .pwm_led_1_b (pwm_led_1_b)
+);
 
 endmodule
