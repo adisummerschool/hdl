c14c049f13408cdcc09a0494d5ce48c5c65df6f6
Cristian Mihai Popa <cristianmihai.popa@analog.com>
2026-07-23 13:52:36 +0300

    library/verilog_task_testbench: Created a module and a testbench for it
    - The main goal was to create a module that generates a saw-tooth like signal
    and to create a testbench for the module, in order to test it
    - The main module(verilog_task_testbench.v) contains a 12b counter that goes up to
    it's maximum value, counts back to zero and then it goes back, on repeat. We use this
    counter to generate a triangle like waveform (seen in analog waveform representaion).
    - Imported the module in a testbench. In here, we emulate the clock and reset signal
    
    Signed-off-by: Cristian Mihai Popa <cristianmihai.popa@analog.com>

--- library/verilog_task_testbench/verilog_task_testbench.v
+++ library/verilog_task_testbench/verilog_task_testbench.v
@@ -43,4 +43,30 @@ module verilog_task_testbench (
   output   [11:0]  triangle_wave
 );
 
+ reg        dir = 1'b1;
+ reg [11:0] cnt = 12'b0;
+
+ always @(posedge ref_clk)
+ begin
+  if (!rstn) begin
+    cnt <= 12'b0;
+  end else begin
+    if(dir == 1'b1)begin
+      if (cnt < 12'hfff) begin
+        cnt <= cnt + 1'b1;
+      end else begin
+        dir <= 1'b0;
+      end
+    end else begin
+      if (cnt > 12'h000) begin
+        cnt <= cnt - 1'b1;
+      end else begin
+        dir <= 1'b1;
+      end
+  end
+  end
+ end
+
+ assign triangle_wave = cnt;
+
 endmodule
