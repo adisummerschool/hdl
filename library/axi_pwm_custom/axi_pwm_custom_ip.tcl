9b97b19fbad28d3e3fa34aaf2f1bc7034a676ca0
Cristian Mihai Popa <cristianmihai.popa@analog.com>
2026-07-27 11:47:25 +0300

    library/axi_pwm_custom: Added ILAs
    - In order to test the functionality of the PWM module
    in hardware, added ILAs in .tcl script, where the
    data_channels and the pwm outputs are being probed,
    alongside the reset signal
    
    Signed-off-by: Cristian Mihai Popa <cristianmihai.popa@analog.com>

--- library/axi_pwm_custom/axi_pwm_custom_ip.tcl
+++ library/axi_pwm_custom/axi_pwm_custom_ip.tcl
@@ -4,6 +4,27 @@ source ../../scripts/adi_env.tcl
 source $ad_hdl_dir/library/scripts/adi_ip_xilinx.tcl
 
 adi_ip_create axi_pwm_custom
+
+create_ip -name ila -vendor xilinx.com -library ip -version 6.2 -module_name my_ila
+set_property -dict [list CONFIG.C_MONITOR_TYPE {Native}] [get_ips my_ila]
+set_property -dict [list CONFIG.C_NUM_OF_PROBES {13}] [get_ips my_ila]
+set_property -dict [list CONFIG.C_DATA_DEPTH {4096}] [get_ips my_ila]
+set_property -dict [list CONFIG.C_TRIGIN_EN {false}] [get_ips my_ila]
+set_property -dict [list CONFIG.C_PROBE0_WIDTH {1}] [get_ips my_ila]
+set_property -dict [list CONFIG.C_PROBE1_WIDTH {12}] [get_ips my_ila]
+set_property -dict [list CONFIG.C_PROBE2_WIDTH {12}] [get_ips my_ila]
+set_property -dict [list CONFIG.C_PROBE3_WIDTH {12}] [get_ips my_ila]
+set_property -dict [list CONFIG.C_PROBE4_WIDTH {12}] [get_ips my_ila]
+set_property -dict [list CONFIG.C_PROBE5_WIDTH {12}] [get_ips my_ila]
+set_property -dict [list CONFIG.C_PROBE6_WIDTH {12}] [get_ips my_ila]
+set_property -dict [list CONFIG.C_PROBE7_WIDTH {1}] [get_ips my_ila]
+set_property -dict [list CONFIG.C_PROBE8_WIDTH {1}] [get_ips my_ila]
+set_property -dict [list CONFIG.C_PROBE9_WIDTH {1}] [get_ips my_ila]
+set_property -dict [list CONFIG.C_PROBE10_WIDTH {1}] [get_ips my_ila]
+set_property -dict [list CONFIG.C_PROBE11_WIDTH {1}] [get_ips my_ila]
+set_property -dict [list CONFIG.C_PROBE12_WIDTH {1}] [get_ips my_ila]
+generate_target {all} [get_files axi_pwm_custom.srcs/sources_1/ip/my_ila/my_ila.xci]
+
 adi_ip_files axi_pwm_custom [list \
   "$ad_hdl_dir/library/common/up_axi.v" \
   "$ad_hdl_dir/library/common/up_xfer_cntrl.v" \
