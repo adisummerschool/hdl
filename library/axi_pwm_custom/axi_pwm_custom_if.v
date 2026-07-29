8e597d5a665f6ef363ebec7090db2c2a8097715b
Tomescu Lucas <fanaticlucas98@gmail.com>
2026-07-29 17:10:29 +0300

    library/axi_pwm_custom: PWM signals generator from accelerometer signals
    -The goal was to generate 6 PWM signals from an accelerometer (X+-,Y+-,Z+-) to control the color of 2 RGB LEDs.
    -The implementation adds a PWM period counter, updates the channel values at the end of each period,
     and generates six PWM outputs.
    -Future implementation: data flows from the accelerometers ADC (Analog to Digital Converter)  to the PS (processing system) which
     converts the data from SPI (Serial Peripheral Interface) to the AXI protocol.
    
    Signed-off-by: Tomescu Lucas <fanaticlucas98@gmail.com>

--- library/axi_pwm_custom/axi_pwm_custom_if.v
+++ library/axi_pwm_custom/axi_pwm_custom_if.v
@@ -55,22 +55,68 @@ module axi_pwm_custom_if (
 );
 
   localparam PULSE_PERIOD = 4095;
-
 // internal registers
+  reg [11:0] triangle;
+
+  reg [11:0] channel_0_valid;
+  reg [11:0] channel_1_valid;
+  reg [11:0] channel_2_valid;
+  reg [11:0] channel_3_valid;  
+  reg [11:0] channel_4_valid;
+  reg [11:0] channel_5_valid;   
+
+  reg reg_pwm_led_0;
+  reg reg_pwm_led_1;
+  reg reg_pwm_led_2;
+  reg reg_pwm_led_3;
+  reg reg_pwm_led_4;
+  reg reg_pwm_led_5;
+
+  reg end_of_period;
 
   /*here*/
 
 // internal wires
-
+always @(posedge pwm_clk) begin
+  if (end_of_period || !rstn) begin
+        channel_0_valid <= data_channel_0;
+        channel_1_valid <= data_channel_1;
+        channel_2_valid <= data_channel_2;
+        channel_3_valid <= data_channel_3;
+        channel_4_valid <= data_channel_4;
+        channel_5_valid <= data_channel_5;
+    end
+end
   /*here*/
 
 // generate a signal named end_of_period which has '1' logic value at the end of the signal period
 
   /*here*/
-
+  always @(posedge pwm_clk) begin
+   if(!rstn) begin
+   end_of_period <= 1'b0;
+   end
+    else if(triangle == PULSE_PERIOD) begin
+   end_of_period <= 1'b1;
+    end
+    else begin
+   end_of_period <= 1'b0;
+    end
+  end
 // Create a counter from 0 to PULSE_PERIOD
 
   /*here*/
+  always @(posedge pwm_clk) begin
+    if(!rstn) begin
+      triangle <= 12'h000;
+    end
+    else if(triangle < PULSE_PERIOD) begin
+      triangle <= triangle + 12'h001;
+    end
+    else begin
+      triangle <= 12'h000;
+    end
+  end 
 
 // control the pwm signal value based on the input signal and counter value
 
@@ -81,7 +127,53 @@ module axi_pwm_custom_if (
   /*here*/
 
 // continous assigment of the correct PWM value for the LEDs
+ always @(posedge pwm_clk) begin
+  
+  if(triangle < channel_0_valid || !rstn) begin
+    reg_pwm_led_0 <= 1'b1;
+  end
+  else begin
+    reg_pwm_led_0 <= 1'b0;
+  end
 
+    if(triangle < channel_1_valid || !rstn) begin
+    reg_pwm_led_1 <= 1'b1;
+    
+  end
+  else begin
+    reg_pwm_led_1 <= 1'b0;
+  end
+    if(triangle < channel_2_valid || !rstn) begin
+    reg_pwm_led_2 <= 1'b1;
+  end
+  else begin
+    reg_pwm_led_2 <= 1'b0;
+  end  
+  if(triangle < channel_3_valid || !rstn) begin
+    reg_pwm_led_3 <= 1'b1;
+  end
+  else begin
+    reg_pwm_led_3 <= 1'b0;
+  end  
+  if(triangle < channel_4_valid || !rstn) begin
+    reg_pwm_led_4 <= 1'b1;
+  end
+  else begin
+    reg_pwm_led_4 <= 1'b0;
+  end
+    if(triangle < channel_5_valid || !rstn) begin
+    reg_pwm_led_5 <= 1'b1;
+  end
+  else begin
+    reg_pwm_led_5 <= 1'b0;
+  end
+ end
  /*here*/
+  assign pwm_led_0 = reg_pwm_led_0;
+  assign pwm_led_1 = reg_pwm_led_1;
+  assign pwm_led_2 = reg_pwm_led_2;
+  assign pwm_led_3 = reg_pwm_led_3;
+  assign pwm_led_4 = reg_pwm_led_4;
+  assign pwm_led_5 = reg_pwm_led_5;
 
 endmodule
