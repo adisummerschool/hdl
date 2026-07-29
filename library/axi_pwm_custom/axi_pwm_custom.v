d264c1b8646dd865893742b7082969e00146fb25
Klampfl Helmut <helmutklampfl334@gmail.com>
2026-07-29 17:12:37 +0300

    library/axi_pwm_custom: PWM module testbench
    -created the axi_pwm_custom_if module; it compares signals from our accelerometer output through the PS and creates our PWM saw-tooth signal with that as a reference --  6 output signals total for our 2 leds, each having 3 color channels (RBG);
    -created an instance and connected our module in axi_pwm_custom.v;
    
    Signed-off-by: Klampfl Helmut <helmutklampfl334@gmail.com>

--- library/axi_pwm_custom/axi_pwm_custom.v
+++ library/axi_pwm_custom/axi_pwm_custom.v
@@ -41,12 +41,13 @@ module axi_pwm_custom #(
   
 // 1.Create the pwm output signals 
 
-  /*here*/
-  /*here*/
-  /*here*/
-  /*here*/
-  /*here*/
-  /*here*/
+  output  reg                pwm_led1_r,
+  output  reg                pwm_led1_g,
+  output  reg                pwm_led1_b,
+  output  reg                pwm_led2_r,
+  output  reg                pwm_led2_g,
+  output  reg                pwm_led2_b,
+  
 
   // axi interface
 
@@ -72,6 +73,8 @@ module axi_pwm_custom #(
   output      [31:0]      s_axi_rdata,
   input                   s_axi_rready
 );
+
+
 
   // internal registers
   
@@ -183,7 +186,7 @@ module axi_pwm_custom #(
     .adc_status_header ('b0),
     .adc_crc_err ('b0),
     .adc_softspan (),
-    .adc_data_channel (/*here*/),  // Connect the samples data for CHANNEL 0 
+    .adc_data_channel (data_channel_1),  // Connect the samples data for CHANNEL 0 
     .up_adc_crc_err (),
     .up_adc_pn_err (),
     .up_adc_pn_oos (),
@@ -237,7 +240,7 @@ module axi_pwm_custom #(
     .adc_status_header ('b0),
     .adc_crc_err ('b0),
     .adc_softspan (),
-    .adc_data_channel (/*here*/),  // Connect the samples data for CHANNEL 1 
+    .adc_data_channel (data_channel_1),  // Connect the samples data for CHANNEL 1 
     .up_adc_crc_err (),
     .up_adc_pn_err (),
     .up_adc_pn_oos (),
@@ -291,7 +294,7 @@ module axi_pwm_custom #(
     .adc_status_header ('b0),
     .adc_crc_err ('b0),
     .adc_softspan (),
-    .adc_data_channel (/*here*/),  // Connect the samples data for CHANNEL 2 
+    .adc_data_channel (data_channel_2),  // Connect the samples data for CHANNEL 2 
     .up_adc_crc_err (),
     .up_adc_pn_err (),
     .up_adc_pn_oos (),
@@ -345,7 +348,7 @@ module axi_pwm_custom #(
     .adc_status_header ('b0),
     .adc_crc_err ('b0),
     .adc_softspan (),
-    .adc_data_channel (/*here*/),  // Connect the samples data for CHANNEL 3
+    .adc_data_channel (data_channel_3),  // Connect the samples data for CHANNEL 3
     .up_adc_crc_err (),
     .up_adc_pn_err (),
     .up_adc_pn_oos (),
@@ -399,7 +402,7 @@ module axi_pwm_custom #(
     .adc_status_header ('b0),
     .adc_crc_err ('b0),
     .adc_softspan (),
-    .adc_data_channel (/*here*/),  // Connect the samples data for CHANNEL 4 
+    .adc_data_channel (data_channel_4),  // Connect the samples data for CHANNEL 4 
     .up_adc_crc_err (),
     .up_adc_pn_err (),
     .up_adc_pn_oos (),
@@ -453,7 +456,7 @@ module axi_pwm_custom #(
     .adc_status_header ('b0),
     .adc_crc_err ('b0),
     .adc_softspan (),
-    .adc_data_channel (/*here*/),  // Connect the samples data for CHANNEL 5
+    .adc_data_channel (data_channel_5),  // Connect the samples data for CHANNEL 5
     .up_adc_crc_err (),
     .up_adc_pn_err (),
     .up_adc_pn_oos (),
@@ -486,6 +489,20 @@ module axi_pwm_custom #(
   // Add the interface module and connect it to the samples data and output ports
   axi_pwm_custom_if i_if_axi_pwm_custom (
     /*here*/
+    .pwm_clk(pwm_clk),
+    .rstn(up_rstn),
+    .data_channel_0(data_channel_0),
+    .data_channel_1(data_channel_1),
+    .data_channel_2(data_channel_2),
+    .data_channel_3(data_channel_3),
+    .data_channel_4(data_channel_4),
+    .data_channel_5(data_channel_5),
+    .pwm_led_0(pwm_led1_r),
+    .pwm_led_1(pwm_led1_g),
+    .pwm_led_2(pwm_led1_b),
+    .pwm_led_3(pwm_led2_r),
+    .pwm_led_4(pwm_led2_g),
+    .pwm_led_5(pwm_led2_b),
   );
 
 endmodule
--- library/axi_pwm_custom/axi_pwm_custom_if.v
+++ library/axi_pwm_custom/axi_pwm_custom_if.v
@@ -59,29 +59,86 @@ module axi_pwm_custom_if (
 // internal registers
 
   /*here*/
+  reg end_of_period;
+  reg [11:0] channel_0;
+  reg [11:0] channel_1;
+  reg [11:0] channel_2;
+  reg [11:0] channel_3;
+  reg [11:0] channel_4;
+  reg [11:0] channel_5;
+  reg [11:0] counter;
+  reg comp0;
+  reg comp1;
+  reg comp2;
+  reg comp3;
+  reg comp4;
+  reg comp5;
+
+  
 
 // internal wires
-
-  /*here*/
+  
+  ///
 
 // generate a signal named end_of_period which has '1' logic value at the end of the signal period
 
-  /*here*/
+always@(posedge pwm_clk) begin   ///////////
+
+  if (counter == 12'hFFF) begin
+  end_of_period <= 1;
+  end
+  else begin
+  end_of_period <= 0;
+  end
 
 // Create a counter from 0 to PULSE_PERIOD
 
-  /*here*/
-
+  if(~rstn) begin
+    counter <= 1'b0;
+    comp0 <= 1;
+    comp1 <= 1;
+    comp2 <= 1;
+    comp3 <= 1;
+    comp4 <= 1;
+    comp5 <= 1;
+  end 
+  else begin
+    counter <= (counter < PULSE_PERIOD) ? counter + 1'b1 : 12'b0;
+  end
+  
 // control the pwm signal value based on the input signal and counter value
 
-  /*here*/
+  if (rstn) begin
+  comp0 <= (channel_0 > counter) ? 1'b1 : 1'b0;
+  comp1 <= (channel_1 > counter) ? 1'b1 : 1'b0;
+  comp2 <= (channel_2 > counter) ? 1'b1 : 1'b0;
+  comp3 <= (channel_3 > counter) ? 1'b1 : 1'b0;
+  comp4 <= (channel_4 > counter) ? 1'b1 : 1'b0;
+  comp5 <= (channel_5 > counter) ? 1'b1 : 1'b0;
+  end
+
+end  // end always posedge clk   ///////////////
 
 // make sure that the new data is processed only after the END_OF_PERIOD
 
-  /*here*/
+  always@(posedge pwm_clk) begin
+    if(end_of_period == 1 || ~rstn) begin 
+      channel_0 <= data_channel_0;
+      channel_1 <= data_channel_1;
+      channel_2 <= data_channel_2;
+      channel_3 <= data_channel_3;
+      channel_4 <= data_channel_4;
+      channel_5 <= data_channel_5;
+    end
+  end
 
 // continous assigment of the correct PWM value for the LEDs
 
- /*here*/
+assign pwm_led_0 = comp0;
+assign pwm_led_1 = comp1;
+assign pwm_led_2 = comp2;
+assign pwm_led_3 = comp3;
+assign pwm_led_4 = comp4;
+assign pwm_led_5 = comp5;
 
 endmodule
