2fc1268b911e8ac1a371c64a510d3db446313bee
Denisa Stanut <stanutdenisa099@gmail.com>
2026-07-30 15:56:22 +0300

    projects/ad5592r_pmdz: Cora Z7 Project Files
    - Integrated the PWM module in the project
    - common/ad5592r_pmdz_bd.tcl: - added the 6 PWM led ports
    			      - instantiated the axi_pwm_custom core
    			      - connected top-level port to the corresponding output pin of the PWM IP that we created
    - coraz7s/system_top.v: - added the PWM wires and connected them to the RGB LEDs
    			- added the SPI signals for the AD5592 PMOD and the sniffing PMOD connected to the SPI signals
    - coraz7s/system_constr.xdc: - added the constraints file for mapping the SPI and the sniffing SPI signals (we followed the Cora Z7 datasheet)
    
    Signed-off-by: Denisa Stanut <stanutdenisa099@gmail.com>

--- projects/ad5592r_pmdz/common/ad5592r_pmdz_bd.tcl
+++ projects/ad5592r_pmdz/common/ad5592r_pmdz_bd.tcl
@@ -37,32 +37,32 @@
 # 1. Create output ports for the PWM-driven RGB LEDs
 #    These ports will be mapped to FPGA pins in the constraints file (.xdc).
 
-  #here
-  #here
-  #here
+  create_bd_port -dir O pwm_led1_r
+  create_bd_port -dir O pwm_led1_g
+  create_bd_port -dir O pwm_led1_b
 
-  #here
-  #here
-  #here
+  create_bd_port -dir O pwm_led2_r
+  create_bd_port -dir O pwm_led2_g
+  create_bd_port -dir O pwm_led2_b
 
 # 2. Instantiate the axi_pwm_custom IP core.
 #    This creates a block named "axi_pwm_custom" in the design using
 #    the axi_pwm_custom IP (defined in library/axi_pwm_custom).
 
-  #here
+  ad_ip_instance axi_pwm_custom pwm_custom_ip
 
 # 3. Connect each top-level port to the corresponding output pin
 #    of the axi_pwm_custom IP.
 
-  #here
-  #here
-  #here
+  ad_connect pwm_custom_ip/pwm_led1_r pwm_led1_r
+  ad_connect pwm_custom_ip/pwm_led1_g pwm_led1_g
+  ad_connect pwm_custom_ip/pwm_led1_b pwm_led1_b
 
-  #here
-  #here
-  #here
+  ad_connect pwm_custom_ip/pwm_led2_r pwm_led2_r
+  ad_connect pwm_custom_ip/pwm_led2_g pwm_led2_g
+  ad_connect pwm_custom_ip/pwm_led2_b pwm_led2_b
 
 # 4. Connect the axi_pwm_custom IP to the CPU's AXI bus at address 0x44A00000.
 #    This lets software read/write the IP's registers from the PS (processing system).
 
-  #here
+  ad_cpu_interconnect 0x44A00000 pwm_custom_ip
--- projects/ad5592r_pmdz/coraz7s/system_top.v
+++ projects/ad5592r_pmdz/coraz7s/system_top.v
@@ -67,15 +67,15 @@ module system_top (
   inout   [ 5:0]  led,
 
 // 4. Add SPI ports for both ADC PMOD connector and sniffing PMOD connector 
-  /*here*/
-  /*here*/
-  /*here*/
-  /*here*/
+  output          spi0_clk_o,   // CLK
+  output          spi0_csn_0_o, // CS
+  input           spi0_sdi_i,   // MISO
+  output          spi0_sdo_o,   // MOSI
 
-  /*here*/
-  /*here*/
-  /*here*/
-  /*here*/
+  output          sniff_spi0_clk_o,    
+  output          sniff_spi0_csn_0_o, 
+  output          sniff_spi0_sdi_i,  
+  output          sniff_spi0_sdo_o 
 );
 
   // internal signals
@@ -86,13 +86,13 @@ module system_top (
  
 // 3. Declare the PWM wires that controls the LED's
 
-  /*here*/
-  /*here*/
-  /*here*/
+  wire            pwm_led1_r;
+  wire            pwm_led1_g;
+  wire            pwm_led1_b;
   
-  /*here*/
-  /*here*/
-  /*here*/
+  wire            pwm_led2_r;
+  wire            pwm_led2_g;
+  wire            pwm_led2_b;
 
   // instantiations
   ad_iobuf #(
@@ -108,12 +108,12 @@ module system_top (
   ) i_iobuf_leds (
     .dio_t (2'h0),
 // 2. Connect the PWM wires to the input port of the ad_iobuf
-    .dio_i ({/*here*/           //led[5]
-             /*here*/           //led[4]
-             /*here*/           //led[3]
-             /*here*/           //led[2]
-             /*here*/           //led[1]
-             /*here*/}),        //led[0]
+    .dio_i ({pwm_led2_g,           //led[5]
+             pwm_led2_r,           //led[4]
+             pwm_led2_b,           //led[3]
+             pwm_led1_g,           //led[2]
+             pwm_led1_r,           //led[1]
+             pwm_led1_b}),         //led[0]
     .dio_o (gpio_i[7:2]),
     .dio_p (led));
 
@@ -122,10 +122,10 @@ module system_top (
 
 // 6. Clone the ADC SPI port to the sniffing ports 
 
-  /*here*/
-  /*here*/
-  /*here*/
-  /*here*/
+  assign sniff_spi0_clk_o = spi0_clk_o;
+  assign sniff_spi0_csn_0_o = spi0_csn_0_o;
+  assign sniff_spi0_sdi_i = spi0_sdi_i;
+  assign sniff_spi0_sdo_o = spi0_sdo_o;
 
   system_wrapper i_system_wrapper (
     .ddr_addr (ddr_addr),
@@ -156,14 +156,14 @@ module system_top (
     .gpio_t (gpio_t),
 
     .spi0_clk_i (),
-    .spi0_clk_o (/*here*/),   // 5. Connect here the SPI CLK
-    .spi0_csn_0_o (/*here*/),   // 5. Connect here the SPI CS
+    .spi0_clk_o (spi0_clk_o),   // 5. Connect here the SPI CLK
+    .spi0_csn_0_o (spi0_csn_0_o),   // 5. Connect here the SPI CS
     .spi0_csn_1_o (),
     .spi0_csn_2_o (),
     .spi0_csn_i (1'b1),
-    .spi0_sdi_i (/*here*/),   // 5. Connect here the SPI MISO
+    .spi0_sdi_i (spi0_sdi_i),   // 5. Connect here the SPI MISO
     .spi0_sdo_i (),
-    .spi0_sdo_o (/*here*/),   // 5. Connect here the SPI MOSI
+    .spi0_sdo_o (spi0_sdo_o),   // 5. Connect here the SPI MOSI
     .spi1_clk_i (1'b0),
     .spi1_clk_o (),
     .spi1_csn_0_o (),
@@ -176,11 +176,11 @@ module system_top (
     .iic_ard_scl_io (iic_ard_scl),
     .iic_ard_sda_io (iic_ard_sda),
 // 1. Declare the block design ports and connect them to the PWM wires 
-    /*here*/
-    /*here*/
-    /*here*/
-    /*here*/
-    /*here*/
-    /*here*/);
+    .pwm_led1_r(pwm_led1_r),
+    .pwm_led1_g(pwm_led1_g),
+    .pwm_led1_b(pwm_led1_b),
+    .pwm_led2_r(pwm_led2_r),
+    .pwm_led2_g(pwm_led2_g),
+    .pwm_led2_b(pwm_led2_b));
 
 endmodule
